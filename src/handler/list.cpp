7fffa4599a0608874c9913f4073e26e3fde82e08
Remi Aleksander Solås <remi@npolar.no>
2015-01-05 16:48:19 +0100

    Updated list method to list information about polygon ring segments instead of points

--- src/handler/list.cpp
+++ src/handler/list.cpp
@@ -42,23 +42,27 @@ namespace reshp
                 unsigned points = 0;
                 
                 for(unsigned r = 0; r < rings; ++r)
-                    points += poly.rings[r].points.size();
-                
-                printf(":%*s%5i point%c", int(13 - strlen(type)), " ", points, (points == 1 ? ' ' : 's'));
-                printf(", %5i ring%c", rings, (rings == 1 ? ' ' : 's'));
-                printf("\n");
+                    points += poly.rings[r].segments.size();
                 
                 if(full)
                 {
+                    printf(": %i point%c", points, (points == 1 ? ' ' : 's'));
+                    printf(", %i ring%c\n", rings, (rings == 1 ? ' ' : 's'));
+                    
                     for(unsigned r = 0; r < poly.rings.size(); ++r)
                     {
-                        printf("    ring %i (%s, %lu points):\n", r, (poly.rings[r].type == reshp::polygon::ring::inner ? "inner" : "outer"), poly.rings[r].points.size());
+                        printf("    ring %i (%s, %lu segments):\n", r, (poly.rings[r].type == reshp::polygon::ring::inner ? "inner" : "outer"), poly.rings[r].segments.size());
                         
-                        for(unsigned p = 0; p < poly.rings[r].points.size(); ++p)
+                        for(unsigned s = 0; s < poly.rings[r].segments.size(); ++s)
                         {
-                            printf("      [%.4f, %.4f]\n", poly.rings[r].points[p].x, poly.rings[r].points[p].y);
+                            printf("      [%8.4f, %8.4f] -> [%8.4f, %8.4f]\n", poly.rings[r].segments[s].start.x, poly.rings[r].segments[s].start.y, poly.rings[r].segments[s].end.x, poly.rings[r].segments[s].end.y);
                         }
                     }
+                }
+                else
+                {
+                    printf(":%*s%5i point%c", int(13 - strlen(type)), " ", points, (points == 1 ? ' ' : 's'));
+                    printf(", %5i ring%c\n", rings, (rings == 1 ? ' ' : 's'));
                 }
             }
             else printf("\n");
